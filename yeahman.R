966e00201cc3ae147c16288bd54d43d17c519eeb
kevin ledezma <149916052+kevin-ledezma@users.noreply.github.com>
2024-04-10 11:04:40 -0400

    Update yeahman.R

--- yeahman.R
+++ yeahman.R
@@ -3,7 +3,7 @@ rep(x, times=550)
 df <- data.frame(
   x = 1, y = 1, 
   label = paste(
-    "<span style='color: red; font-size: 100pt'>hell yeah, man!</span>,",
+    "<span style='color: blue; font-size: 90pt'>hell yeah, hermano!!</span>,",
     "<span style='color: black; font-size: 20pt'>I love R.</span>", sep = "<br>"))
 
 ggplot(df) +
