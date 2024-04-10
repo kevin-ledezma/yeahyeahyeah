9decbfcbd8249909fbca300d77be1409a038e45d
kevin ledezma <149916052+kevin-ledezma@users.noreply.github.com>
2024-04-10 10:47:42 -0400

    Update yeahman.R

--- yeahman.R
+++ yeahman.R
@@ -1,2 +1,11 @@
 x<-"yeah man! I love R"
 rep(x, times=550)
+df <- data.frame(
+  x = x, y = 1, 
+  label = paste(
+    "<span style='color: red; font-size: 100pt'>hell yeah, man!</span>,",
+    "<span style='color: black; font-size: 20pt'>I love R.</span>", sep = "<br>"))
+
+ggplot(df) +
+  ggtext::geom_textbox(aes(x = x, y = y, label = label), box.colour = NA, width = unit(10, "cm")) +
+  theme_void()
